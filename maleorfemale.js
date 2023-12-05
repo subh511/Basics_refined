7365ad92abaa594c81240dfb7ea4fa4379cd4e61
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-05 18:12:28 +0530

    genders

---
+++ maleorfemale.js
@@ -0,0 +1,9 @@
+let personArray = ["Samrat","Subh","Nikki"];
+let genderArray = ["male","male","female"];
+
+
+for(let i=0; i<personArray.length; i++){
+    if(genderArray[i] == "male"){
+        console.log(personArray[i])
+    }
+}
