7267f3f99f2578742a5021231f15dd393b1364ef
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-05 13:31:28 +0530

    second commit

--- practise.js
+++ practise.js
@@ -3,4 +3,13 @@ let age = 25;
 let isMarried = false;
 
 
-console.log("My Name is" + " " + firstName + " and my age" + " " + age);
+// console.log("My Name is" + " " + firstName + " and my age" + " " + age);
+
+
+// if and else statement;
+
+if(!isMarried){
+    console.log(firstName + " " + "not married");
+}else{
+    console.log(firstName + " " + "is married");
+}
