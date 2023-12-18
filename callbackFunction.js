e9b6a83bc9a3f557d0fa34d1c866187959b6e043
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-18 13:54:38 +0530

    added more

--- callbackFunction.js
+++ callbackFunction.js
@@ -1,14 +1,34 @@
-function square(n){
-    return n*n
+// function square(n){
+//     return n*n
+// }
+
+// function sumOfSquare(a,b){
+
+//     let val1 = square(a);
+//     let val2 = square(b);
+//     return val1 + val2;
+// }
+
+function cube(n) {
+  return n * n * n;
+}
+function square(n) {
+  return n * n;
 }
 
-
-function sumOfSquare(a,b){
-
-    let val1 = square(a);
-    let val2 = square(b);
-    return val1 + val2;
+function sumOfSquare(a, b) {
+  let val1 = square(a);
+  let val2 = square(b);
+  return val1 + val2;
 }
 
-const ans = sumOfSquare(2,6);
-console.log(ans);
+function sumOfCubes(a, b) {
+  let val1 = cube(a);
+  let val2 = cube(b);
+  return val1 + val2;
+}
+
+const ans1 = sumOfSquare(2, 6);
+console.log(ans1);
+const ans2 = sumOfCubes(2, 3);
+console.log(ans2)
