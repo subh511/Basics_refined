69185bae5530081d5094386fd84cf301e3c2462e
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-22 11:38:57 +0530

    modified

--- async function/setTimeOut.js
+++ async function/setTimeOut.js
@@ -1,12 +1,30 @@
-console.log("hi there!");
+// console.log("hi there!");
 
-setTimeout(function(){
-        console.log("from inside async fn")
-    },5000);
+// setTimeout(function(){
+//         console.log("from inside async fn")
+//     },5000);
     
-    let a=0;
-    for(let i=0; i<10; i++){
-        a += i;
+//     let a=0;
+//     for(let i=0; i<10; i++){
+//         a += i;
+//     }
+    
+//     console.log(a);
+
+
+    function cube(n){
+        return n*n*n;
     }
-    
-    console.log(a);
+
+    function square(n){
+        return n*n;
+    }
+
+    function getProductOfAll(a,b,fn){
+        let val1 = fn(a);
+        let val2 = fn(b);
+        return val1+val2;
+    }
+
+    console.log(getProductOfAll(6,2,cube))
+    console.log(getProductOfAll(6,2,square))
