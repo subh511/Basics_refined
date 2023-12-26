4d299c3b252544b9a3aed36d9839b0e27ae37cc6
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-26 21:21:26 +0530

    basic.js

---
+++ map, filter & arrow fns/basic.js
@@ -0,0 +1,13 @@
+
+//map,filter,arrow fns as below;
+//given a new array, return new array with the value multiply with 2
+
+let array = [1,2,3,4,5,6];
+
+let newArray = [];
+
+
+for(let i=0; i<array.length; i++){
+    newArray.push(array[i]*2)
+}
+console.log(newArray)
