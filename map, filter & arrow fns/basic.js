1c710151f530469853399c29073d48dcf8944aea
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-12 12:05:01 +0530

    created counter through js

--- map, filter & arrow fns/basic.js
+++ map, filter & arrow fns/basic.js
@@ -1,13 +1,25 @@
-
 //map,filter,arrow fns as below;
 //given a new array, return new array with the value multiply with 2
 
-let array = [1,2,3,4,5,6];
+let array = [1, 2, 3, 4, 5, 6];
 
 let newArray = [];
 
-
-for(let i=0; i<array.length; i++){
-    newArray.push(array[i]*2)
+for (let i = 0; i < array.length; i++) {
+  newArray.push(array[i] * 2);
 }
-console.log(newArray)
+console.log(newArray);
+
+// in string notation we see as below;
+
+let names = ["sam", "ram", "tam"];
+
+let filteredMethod = names.filter(function (n) {
+  if (n.startsWith("s")) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(filteredMethod);
---
+++ map, filter & arrow fns/practise.js
@@ -0,0 +1,2 @@
+//create a map fn that takes an array and a tranform function 
+//as input and returns the tranformed array as o/p;
